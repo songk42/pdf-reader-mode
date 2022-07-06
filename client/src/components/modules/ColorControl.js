a384684375530651f2da0bcf277331c1d58ed953
Song Kim <songkimwl@gmail.com>
2022-07-05 22:09:24 -0400

    add underline on hovering over font/color buttons

--- client/src/components/modules/ColorControl.js
+++ client/src/components/modules/ColorControl.js
@@ -5,12 +5,15 @@ import "./Menu.css";
 function ColorControl(props) {
     const colorLower = props.color.toLowerCase();
     return (
-        <button
-            className={`menu-color${props.colorScheme==colorLower ? " menu-color-selected" : " "} menu-${colorLower}-button`}
-            onClick={(e) => props.setColorScheme(colorLower)}
-        >
-            <label>{props.color}</label>
-        </button>
+        <div className="menu-color-container">
+            <button
+                className={`menu-color${props.colorScheme==colorLower ? " menu-color-selected" : " "} menu-${colorLower}-button`}
+                onClick={(e) => props.setColorScheme(colorLower)}
+            >
+                <label>{props.color}</label>
+            </button>
+            <div className="menu-color-underline"></div>
+        </div>
     );
 }
 
--- client/src/components/modules/FontControl.js
+++ client/src/components/modules/FontControl.js
@@ -4,13 +4,16 @@ import "./Menu.css";
 
 function FontControl(props) {
     return (
-        <button
-            className={`menu-font menu-${props.font.toLowerCase()}${props.serif == (props.font == "Serif") ? " menu-font-selected" : ""}`}
-            style={{ backgroundImage: `url(${props.bg})` }}
-            onClick={(e) => props.setSerif(props.font == "Serif")}
-        >
-            <label>{props.font}</label>
-        </button>
+        <div className="menu-font-container">
+            <button
+                className={`menu-font menu-${props.font.toLowerCase()}${props.serif == (props.font == "Serif") ? " menu-font-selected" : ""}`}
+                style={{ backgroundImage: `url(${props.bg})` }}
+                onClick={(e) => props.setSerif(props.font == "Serif")}
+            >
+                <label>{props.font}</label>
+            </button>
+            <div className="menu-font-underline"></div>
+        </div>
     );
 }
 
